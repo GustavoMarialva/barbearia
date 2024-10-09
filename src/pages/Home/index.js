efc11ae506e24d43af9f8ba574b7b4acd608f841
Gustavo Marialva <gmarialva@gmail.com>
2024-10-09 13:54:21 -0300

    renderiza dados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "../../components/Header";
 import Balance from "../../components/Balance";
+import Movements from "../../components/Movements";
+
+const list = [
+  {
+    id: 1,
+    label: "Boleto conta de luz",
+    value: "600,50",
+    date: "10/09/2024",
+    type: 0, //despesas
+  },
+
+  {
+    id: 2,
+    label: "Pix Cliente X",
+    value: "110,00",
+    date: "12/09/2024",
+    type: 1, //receita
+  },
+
+  {
+    id: 3,
+    label: "Pagamento Cliente Y",
+    value: "65,00",
+    date: "15/09/2024",
+    type: 1, //receita
+  },
+];
 
 export default function Home() {
   return (
@@ -9,6 +36,16 @@ export default function Home() {
       <Header name="Baiano Barber's" />
 
       <Balance saldo="10.000,00" gastos="-600,00" />
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Movements data={item} />}
+      />
     </View>
   );
 }
@@ -17,5 +54,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f6fa",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
+  },
+
+  list: {
+    marginRight: 14,
+    marginLeft: 14,
   },
 });
